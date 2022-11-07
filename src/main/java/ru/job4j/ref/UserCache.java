9ea2a514d53be125f2928fb28455a91c8e3dce47
Dimarik <krasnodar93@mail.ru>
2022-11-07 20:27:08 +0300

    4. Thread без общих ресурсов [#267919 #358081]

--- src/main/java/ru/job4j/ref/UserCache.java
+++ src/main/java/ru/job4j/ref/UserCache.java
@@ -1,18 +1,25 @@
 package ru.job4j.ref;
 
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.atomic.AtomicInteger;
+import java.util.stream.Collectors;
 
 public class UserCache {
     private final ConcurrentHashMap<Integer, User> users = new ConcurrentHashMap<>();
     private final AtomicInteger id = new AtomicInteger();
 
-    public void add(User user) {
+    public synchronized void add(User user) {
         users.put(id.incrementAndGet(), user);
     }
 
-    public User findById(int id) {
+    public synchronized User findById(int id) {
         return users.get(id);
+    }
+
+    public synchronized List<User> findAll() {
+        return users.entrySet().stream().map(i -> i.getValue()).collect(Collectors.toList());
     }
 }
