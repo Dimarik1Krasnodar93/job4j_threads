ba0947aabf1ab1b365bdc6817ac50684fff98a36
Dimarik <krasnodar93@mail.ru>
2022-11-09 10:29:17 +0300

    0. Управление нитью через wait. [#6858 #359219]

---
+++ src/main/java/ru/job4j/waitnotify/CountBarrier.java
@@ -0,0 +1,54 @@
+package ru.job4j.waitnotify;
+
+public class CountBarrier {
+    private final Object monitor = this;
+
+    private final int total;
+
+    private int count = 1000;
+
+    public CountBarrier(final int total) {
+        this.total = total;
+    }
+
+    public synchronized void count() {
+        if (count-- > 0) {
+            try {
+                Thread.sleep(1);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            notifyAll();
+            count();
+        } else {
+            System.out.println("count");
+        }
+    }
+
+    public void await() {
+        while (count >= total) {
+            System.out.println(String.format("%s___Await %c", Thread.currentThread().toString(), count));
+        }
+    }
+
+    public static void main(String[] args) throws Exception {
+        CountBarrier countBarrier1 = new CountBarrier(5);
+        Thread thread1 = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                countBarrier1.count();
+            }
+        });
+        Thread thread2 = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                countBarrier1.await();
+            }
+        });
+
+        thread1.start();
+        thread2.start();
+        thread1.join();
+        thread2.join();
+    }
+}
