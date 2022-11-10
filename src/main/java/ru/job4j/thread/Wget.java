ff7febd910734ed03de3c7b7905fb28d4b8936e6
Dimarik <krasnodar93@mail.ru>
2022-11-10 14:15:48 +0300

    4. Скачивание файла с ограничением. [#144271 #357819]

--- src/main/java/ru/job4j/thread/Wget.java
+++ src/main/java/ru/job4j/thread/Wget.java
@@ -4,8 +4,6 @@ import java.io.BufferedInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.net.URL;
-import java.net.MalformedURLException;
-import java.net.URISyntaxException;
 
 public class Wget implements Runnable {
     private final String url;
@@ -28,8 +26,8 @@ public class Wget implements Runnable {
             int downloadData = 0;
             while ((bytesRead = in.read(dataBuffer, 0, 1024)) != -1) {
                 fileOutputStream.write(dataBuffer, 0, bytesRead);
+                downloadData += bytesRead;
                 if (downloadData >= speed) {
-                    downloadData += bytesRead;
                     end = System.currentTimeMillis();
                     try {
                         if (end - start < 1000) {
@@ -41,7 +39,6 @@ public class Wget implements Runnable {
                         ex.printStackTrace();
                     }
                 }
-                bytesRead = in.read(dataBuffer, 0, 1024);
             }
         } catch (IOException e) {
             e.printStackTrace();
@@ -49,12 +46,14 @@ public class Wget implements Runnable {
     }
 
     public static void main(String[] args) throws InterruptedException {
+        validate(args);
         String url = args[0];
         int speed = Integer.parseInt(args[1]);
         Thread wget = new Thread(new Wget(url, speed));
         wget.start();
         wget.join();
     }
+
     private static void validate(String[] args) {
         if (args.length < 2) {
             throw new IllegalArgumentException("Illegal count of arguments");
