28cace3ae3860e95936bbfc1ca2259898eab8046
Dimarik <krasnodar93@mail.ru>
2022-11-11 21:11:57 +0300

    2. Обеспечить остановку потребителя. [#66825]

---
+++ src/main/java/ru/job4j/buffer/ParallelSearch.java
@@ -0,0 +1,46 @@
+package ru.job4j.buffer;
+
+import ru.job4j.waitnotify.SimpleBlockingQueue;
+
+public class ParallelSearch {
+
+    public static void main(String[] args) {
+        SimpleBlockingQueue<Integer> queue = new SimpleBlockingQueue<>(10);
+        final Thread consumer = new Thread(
+                () -> {
+                    synchronized (queue) {
+                        while (true) {
+                            try {
+                                System.out.println(queue.poll());
+                            } catch (InterruptedException e) {
+                                e.printStackTrace();
+                                Thread.currentThread().interrupt();
+                            }
+                        }
+                    }
+                }
+        );
+        consumer.start();
+        new Thread(
+                () -> {
+                    synchronized (queue) {
+                        for (int index = 0; index != 3; index++) {
+                            try {
+                                queue.offer(index);
+                            } catch (InterruptedException e) {
+                                e.printStackTrace();
+                            }
+                            try {
+                                Thread.sleep(500);
+                            } catch (InterruptedException e) {
+                                e.printStackTrace();
+                            }
+                        }
+                        queue.notifyAll();
+                        consumer.interrupt();
+                    }
+                }
+
+        ).start();
+    }
+}
